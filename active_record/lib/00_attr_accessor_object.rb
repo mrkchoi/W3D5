944fcf4ab8e034873ab525daa87550d8a68b9938
Kenneth Choi <kennethichoi@gmail.com>
2019-05-31 14:20:57 -0700

    Complete phase 00 custom active record

---
+++ active_record/lib/00_attr_accessor_object.rb
@@ -0,0 +1,14 @@
+class AttrAccessorObject
+  def self.my_attr_accessor(*names)
+    # ...
+    names.each do |name|
+      define_method("#{name}") do
+        return self.instance_variable_get("@#{name}")
+      end
+
+      define_method("#{name}=") do |value|
+        return self.instance_variable_set("@#{name}", value)
+      end
+    end
+  end
+end

cd819876ee32aa91449175112bbf5e6976654b47
Kenneth Choi <kennethichoi@gmail.com>
2019-05-31 16:15:56 -0700

    Complete SQL_object class

--- active_record/lib/01_sql_object.rb
+++ active_record/lib/01_sql_object.rb
@@ -38,15 +38,35 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+
+    self.parse_all(results)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map do |result|
+      self.new(result)
+    end
   end
 
   def self.find(id)
-    # ...
+    result = DBConnection.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        id = ?
+      LIMIT
+        1
+    SQL
+
+    self.parse_all(result).first
   end
 
   def initialize(params = {})
@@ -67,18 +87,45 @@ class SQLObject
   end
 
   def attribute_values
-    # ...
+    attr_vals = self.class.columns.map do |attr_name|
+      self.send(attr_name)
+    end
+    attr_vals
   end
 
   def insert
-    # ...
+    col_names = self.class.columns.join(', ')
+    question_marks = (['?'] * self.class.columns.count).join(', ')
+
+    DBConnection.execute(<<-SQL, self.attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
-    # ...
+    col_names = self.class.columns.map {|col| "#{col} = ?"}
+    col_values = col_names.join(', ')
+    
+    DBConnection.execute(<<-SQL, self.attribute_values, self.id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{col_values}
+      WHERE
+        id = ?
+    SQL
   end
 
   def save
-    # ...
+    if self.id.nil?
+      self.insert
+    else
+      self.update
+    end
   end
 end
